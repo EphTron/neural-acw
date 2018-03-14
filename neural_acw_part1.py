68c7e3bf334c7bce2c305fa1a496b3fa2cebaf2f
Ephra <es.90@gmx.de>
2018-03-14 10:42:18 +0000

    added comments with hints

--- neural_acw_part1.py
+++ neural_acw_part1.py
@@ -10,7 +10,7 @@ def sigmoid(input_value):
 
 
 def step_func(input_value):
-    return 0 if input_value > 0 else 1
+    return 1 if input_value > 0 else 0
 
 
 def create_input_list(data, inputs, idx):
@@ -33,15 +33,15 @@ def main():
     # setup perceptron
     weights = [random.uniform(0.0, 1.0) for _ in range(input_count)]
     bias = random.uniform(-1.0, 1.0)
-    graphs = {'error': [], 'output': [], 'delta-error': []}
-    output_1, output_2 = [], []
+    graphs = {'error': [], 'output': [], 'delta-error': [], 'before': [], 'after': []}
 
     # output before training
     for sample_idx in range(len(data)):
         inputs = create_input_list(data, input_count, sample_idx)
         x = np.dot(np.array(weights), np.array(inputs)) + bias
-        output_1.append(activation(x))
+        graphs['before'].append(activation(x))
 
+    # training
     for sample_idx in range(len(data)):
         inputs = create_input_list(data, input_count, sample_idx)
 
@@ -49,44 +49,50 @@ def main():
         x = np.dot(np.array(weights), np.array(inputs)) + bias
         output = activation(x)
 
-        # backpropagate
+        # adjusting weights
         error = data[sample_idx] - output
         for w_idx, w in enumerate(weights):
             weights[w_idx] = w + learning_rate * error * inputs[w_idx]
 
-        # safe and print calculations
+        # safe calculations for later plotting
         graphs['error'].append(error)
         graphs['output'].append(output)
         graphs['delta-error'].append(abs(output - data[sample_idx]))
 
+    # output after training
     for sample_idx in range(len(data)):
         inputs = create_input_list(data, input_count, sample_idx)
         x = np.dot(np.array(weights), np.array(inputs)) + bias
-        output_2.append(activation(x))
+        graphs['after'].append(activation(x))
 
+    # setup plots for output
     f1, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
     ax1.title.set_text('Perceptron Output before Training')
     ax1.set(xlabel='Samples of track.xls', ylabel='Predicted Position')
     ax1.grid()
-    ax1.plot(output_1, 'r')
-
+    ax1.plot(graphs['before'], 'r')
     ax2.title.set_text('Perceptron Output after Training')
     ax2.set(xlabel='Samples of track.xls', ylabel='Predicted Position')
     ax2.grid()
-    ax2.plot(output_2, 'b')
+    ax2.plot(graphs['after'], 'b')
 
+    # setup plots for errors
     f1, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
     ax1.title.set_text('Perceptron Delta Error')
     ax1.set(xlabel='Samples of track.xls', ylabel='Deviation (Delta Error)')
     ax1.grid()
     ax1.plot(graphs['delta-error'], 'r')
-
     ax2.title.set_text('Perceptron Error Change during Training')
     ax2.set(xlabel='Samples of track.xls', ylabel='Predicted Position')
     ax2.grid()
     ax2.plot(graphs['error'], 'b')
 
     plt.show()
+
+    # Questions:
+    # - Update function correct?
+    # - Better way to plot errors (average)
+    # - Overfitting after too many training epochs
 
 
 if __name__ == "__main__":
