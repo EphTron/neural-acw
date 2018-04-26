0135b65d542fe83c01383500bf4bcb6fa2fb19c9
Ephra <es.90@gmx.de>
2018-04-26 17:16:59 +0200

    adjusted outputs and plots for acw I and II

--- neural_acw_part1.py
+++ neural_acw_part1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import math
 import random
 
@@ -10,7 +11,52 @@ def sigmoid(input_value):
 
 
 def step_func(input_value):
-    return 1 if input_value > 0 else 0
+    return 0 if input_value > 0 else 1
+
+
+class Perceptron:
+    def __init__(self, input_units, activation_function, learning_rate=0.05):
+        """
+        Create simple perceptron with n inputs and one output.
+        Set an activation function and set learn rate
+        Weights and bias get set to a random value
+        :param input_units: int                 # Define the number of inputs needed
+        :param activation_function: function    # Pass activation function f(x) = y
+        :param learning_rate: float             # Define learning rate
+        """
+        self.input_units = input_units
+        self.weights = [random.uniform(0.0, 1.0) for _ in range(input_units)]
+        self.bias = random.uniform(-1.0, 1.0)
+        self.learning_rate = learning_rate
+        self.activation_function = activation_function
+
+    def calc_output(self, inputs):
+        """
+        Calculate output of perceptron
+        :param inputs: list  # length of inputs must be equal to perceptron inputs
+        :return: float       # return output
+        """
+        if len(inputs) is self.input_units:
+            x = np.dot(np.array(self.weights), np.array(inputs)) + self.bias
+            return self.activation_function(x)
+
+    def train(self, inputs, target):
+        """
+        Train perceptron with given inputs and adjust weights based on target.
+        :param inputs: list   # length of inputs must be equal to perceptron inputs
+        :param target: float  # wanted output of the perceptron
+        :return: float        # returns difference between
+        """
+
+        if len(inputs) is self.input_units:
+            # new_weights = [random.uniform(0.0, 1.0) for _ in range(input_units)]
+            output = self.calc_output(inputs)
+            error = target - output
+            # error = math.sqrt(pow(target - output, 2))
+            for idx, w in enumerate(self.weights):
+                self.weights[idx] = w + self.learning_rate * error * inputs[idx]
+                self.bias = self.bias + self.learning_rate * error
+            return output, error
 
 
 def create_input_list(data, inputs, idx):
@@ -26,73 +72,85 @@ def main():
     # read data and transform to list
     xls_data = pd.ExcelFile("track.xls")
     data = [x[0] for x in pd.Series.tolist(xls_data.parse('data1'))]
+    input_units, learn_rate = 4, 0.05
+    activation = sigmoid
+    ac_label = 'Activation f: Step'
+    in_label = 'Input units: ' + str(input_units)
+    lr_label = 'learn rate = ' + str(learn_rate)
+    # create perceptron with step function
+    perceptron = Perceptron(input_units, activation, learning_rate=learn_rate)
 
-    # setup parameters
-    input_count, learning_rate, activation = 4, 0.05, sigmoid
+    # show calculated output of perceptron after training
+    graphs = {'average-error': [], 'best': [], 'output': [], 'delta-error': [], 'o-before': [], 'e-before': [], 'o-after': [],
+              'e-after': []}
 
-    # setup perceptron
-    weights = [random.uniform(0.0, 1.0) for _ in range(input_count)]
-    bias = random.uniform(-1.0, 1.0)
-    graphs = {'error': [], 'output': [], 'delta-error': [], 'before': [], 'after': []}
+    # assess output of data before training
+    for idx, val in enumerate(data):
+        input_vec = create_input_list(data, input_units, idx)
+        value = perceptron.calc_output(input_vec)
+        graphs['o-before'].append(value)
+        graphs['e-before'].append(data[idx])
 
-    # output before training
-    for sample_idx in range(len(data)):
-        inputs = create_input_list(data, input_count, sample_idx)
-        x = np.dot(np.array(weights), np.array(inputs)) + bias
-        graphs['before'].append(activation(x))
+    # setup parameters and train perceptron
+    best = 1
+    epoch_counter, epochs, error, running = 0, 250, 1, True
+    while epoch_counter < epochs and running:
+        indices = list(range(len(data)))
+        random.shuffle(indices)
+        iter_count = 0
+        error_sum = 0
 
-    # training
-    for sample_idx in range(len(data)):
-        inputs = create_input_list(data, input_count, sample_idx)
+        for idx in indices:
+            input_vec = create_input_list(data, input_units, idx)
+            value, error = perceptron.train(input_vec, data[idx])
+            error_sum += abs(error)
+            iter_count += 1
+        graphs['average-error'].append(error_sum / len(data))
+        if (error_sum / len(data)) < best:
+            best = error_sum / len(data)
+        graphs['best'].append(best)
+        print('Epoch ', epoch_counter, ' Best ', best)
+        epoch_counter += 1
 
-        # calc output
-        x = np.dot(np.array(weights), np.array(inputs)) + bias
-        output = activation(x)
+    # show calculated output of perceptron after training
+    for idx, val in enumerate(data):
+        input_vec = create_input_list(data, input_units, idx)
+        value = perceptron.calc_output(input_vec)
+        graphs['o-after'].append(value)
+        graphs['e-after'].append(data[idx])
+        print()
 
-        # adjusting weights
-        error = data[sample_idx] - output
-        for w_idx, w in enumerate(weights):
-            weights[w_idx] = w + learning_rate * error * inputs[w_idx]
-
-        # safe calculations for later plotting
-        graphs['error'].append(error)
-        graphs['output'].append(output)
-        graphs['delta-error'].append(abs(output - data[sample_idx]))
-
-    # output after training
-    for sample_idx in range(len(data)):
-        inputs = create_input_list(data, input_count, sample_idx)
-        x = np.dot(np.array(weights), np.array(inputs)) + bias
-        graphs['after'].append(activation(x))
-
-    # setup plots for output
-    f1, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-    ax1.title.set_text('ACW I: Perceptron Output before Training')
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    ax1.title.set_text('Perceptron Output before Training')
     ax1.set(xlabel='Samples of track.xls', ylabel='Predicted Position')
     ax1.grid()
-    ax1.plot(graphs['before'], 'r')
-    ax2.title.set_text('ACW I: Perceptron Output after Training')
+    ax1.plot(graphs['e-before'], 'r')
+    ax1.plot(graphs['o-before'], 'b')
+    ax2.title.set_text('Perceptron Output after Training (' + str(epochs) + ' Epochs)')
     ax2.set(xlabel='Samples of track.xls', ylabel='Predicted Position')
     ax2.grid()
-    ax2.plot(graphs['after'], 'b')
+    ac = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=ac_label)
+    iu = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=in_label)
+    lr = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=lr_label)
+    p1, = ax2.plot(graphs['e-after'], 'r', label='Real Position')
+    p2, = ax2.plot(graphs['o-after'], 'b', label='Predicted Position')
+    ax2.legend([ac, iu, lr, p2, p1],
+               [ac_label, in_label, lr_label, 'Predicted Position' , 'Real Position'], prop={'size': 14})
 
     # setup plots for errors
-    f1, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-    ax1.title.set_text('ACW I: Perceptron Delta Error')
-    ax1.set(xlabel='Samples of track.xls', ylabel='Deviation (Delta Error)')
+    f2, (ax1) = plt.subplots(1, 1, sharey=True)
+    ax1.title.set_text('ACW I: Perceptron Average Error Improvement')
+    ax1.set(xlabel='Epochs', ylabel='Avg. Error in Epoch')
     ax1.grid()
-    ax1.plot(graphs['delta-error'], 'r')
-    ax2.title.set_text('ACW I: Perceptron Error Change during Training')
-    ax2.set(xlabel='Samples of track.xls', ylabel='Predicted Position')
-    ax2.grid()
-    ax2.plot(graphs['error'], 'b')
+    ac = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=ac_label)
+    iu = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=in_label)
+    lr = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=lr_label)
+    best_error, = ax1.plot(graphs['best'], 'g', label='Lowest NN Error')
+    avg_error, = ax1.plot(graphs['average-error'], 'r', label='Avg. Error of Epoch')
+    ax1.legend([ac,iu, lr, avg_error, best_error],
+               [ac_label, in_label, lr_label, 'Avg. Error of Epoch', 'Lowest NN Error'], prop={'size': 14})
 
     plt.show()
-
-    # Questions:
-    # - Update function correct?
-    # - Better way to plot errors (average)
-    # - Overfitting after too many training epochs
 
 
 if __name__ == "__main__":
--- neural_acw_part2.py
+++ neural_acw_part2.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import random
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 from math import floor
 
 
@@ -29,7 +30,6 @@ class Network:
         print('Layer Count: ', self.layers)
         self.weights = [np.random.normal(0.0, 0.7, (self.strct[idx + 1], self.strct[idx])) for idx in
                         range(len(self.strct) - 1)]
-        # self.weights = [np.random.rand(self.strct[idx + 1], self.strct[idx]) for idx in range(len(self.strct) - 1)]
         self.last_update = [np.zeros((self.strct[idx + 1], self.strct[idx])) for idx in range(len(self.strct) - 1)]
         self.biases = [random.uniform(-1.0, 1.0) for _ in range(len(self.weights))]
         self.inputs = [np.zeros((1, self.strct[idx])) for idx in range(self.layers - 1)]
@@ -85,26 +85,33 @@ class Network:
                 gradient = -errors[k] * derivative * self.inputs[idx][j]
                 update = (learning_rate * gradient + momentum * self.last_update[idx][k, j])
                 new_weights[k, j] = w - update
+
                 layer_error[j] += errors[k] * derivative * w
                 self.last_update[idx][k, j] = update
             weight_updates.append(new_weights)
             errors = layer_error
             self.weights[idx] = new_weights
+            # bias = self.biases[idx] - learning_rate * (np.sum(errors) / len(errors))
+            # self.biases[idx] = bias
 
         return error_sum
 
-    def train(self, epochs, training_data, test_data, learn_rate=0.1, momentum=0.1):
+    def train(self, epochs, data, learn_rate=0.1, momentum=0.1):
+        training_data, test_data = split_to_test_and_training(data, 0.85)
         print('Start training')
         graphs = {'error': [], 'test-error': [], 'wanted-output': [], 'best': [], 'before': [], 'after': []}
         sorted_data = training_data
+
+        # assess fitness before training
         for data_set in sorted_data:
             out = self.make_decision(data_set[:2])
             graphs['before'].append(out[0])
             graphs['wanted-output'].append(data_set[2])
         best_epoch = 1
         best_epoch_num = 0
-        # best_epoch_weights = None
+        best_epoch_weights = None
 
+        # train network
         smallest_error = 1
         for i in range(epochs):
             random.shuffle(training_data)
@@ -119,7 +126,7 @@ class Network:
             if best_epoch > total_error:
                 best_epoch = total_error
                 best_epoch_num = i
-                # best_epoch_weights = self.weight
+                best_epoch_weights = self.weights
 
             test_epoch_error = 0
             for test_set in test_data:
@@ -133,35 +140,48 @@ class Network:
             print('Epoch ', i, ' Best: ', best_epoch, ' Error:', total_error)
 
         print('End training. Best epoch', best_epoch_num, ' Error: ', best_epoch)  # ,'\nWeights: ', best_epoch_weights)
+        self.weights = best_epoch_weights
 
+        # assess fitness with best weights
         for data_set in sorted_data:
             out = self.make_decision(data_set[:2])
             graphs['after'].append(out[0])
+
+        nn_label = 'NN structure: ' + str(self.strct)
+        lr_label = 'learn rate = ' + str(learn_rate)
+        mo_label = 'momentum = ' + str(momentum)
 
         f1, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
         ax1.title.set_text('ACW II: Neural Network before Training')
         ax1.set(xlabel='Set46 Samples', ylabel='Target acquired')
         ax1.grid()
-        ax1.plot(graphs['wanted-output'], 'go')
-        ax1.plot(graphs['before'], 'ro')
-        ax2.title.set_text('ACW II: Neural Network after Training')
+        ax1.plot(graphs['wanted-output'], 'go', label='Target Output')
+        ax1.plot(graphs['before'], 'bo', label='Untrained NN Output')
+
+        ax2.title.set_text('ACW II: Neural Network after Training (' + str(epochs) + ' Epochs)')
         ax2.set(xlabel='Set46 Samples', ylabel='Target acquired')
         ax2.grid()
-        ax2.plot(graphs['wanted-output'], 'go')
-        ax2.plot(graphs['after'], 'bo')
+        nn = ax2.plot([0, 0], 'white', label=nn_label)
+        lr = ax2.plot([0, 0], 'white', label=lr_label)
+        mo = ax2.plot([0, 0], 'white', label=mo_label)
+        ax2.plot(graphs['wanted-output'], 'go', label='Target Output')
+        ax2.plot(graphs['after'], 'bo', label='Trained NN Output')
+        ax2.legend(prop={'size': 14})
 
         # setup plots for errors
-        f1, (ax1) = plt.subplots(1, 1, sharey=True)
-        ax1.title.set_text('ACW II: Neural Network Average Error Improvement')
-        ax1.set(xlabel='Epochs', ylabel='Average MSE')
-        ax1.grid()
-        ax1.plot(graphs['test-error'], 'b')
-        ax1.plot(graphs['best'], 'g')
-        ax1.plot(graphs['error'], 'r')
-        # ax2.title.set_text('ACW II: Perceptron Error Change during Training')
-        # ax2.set(xlabel='Samples of track.xls', ylabel='Predicted Position')
-        # ax2.grid()
-        # ax2.plot(graphs['error'], 'b')
+        f2, (ax3) = plt.subplots(1, 1, sharey=True)
+        ax3.title.set_text('ACW II: Neural Network Average Error Improvement')
+        ax3.set(xlabel='Epochs', ylabel='Average MSE')
+        ax3.grid()
+        nn = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=nn_label)
+        lr = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=lr_label)
+        mo = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0, label=mo_label)
+        test_set_err_plot, = ax3.plot(graphs['test-error'], 'grey', label='Avg. Error of Test Set')
+        lowest_err_plot, = ax3.plot(graphs['best'], 'g', label='Lowest NN Error')
+        train_set_err_plot, = ax3.plot(graphs['error'], 'b', label='Avg. Error of Epoch')
+        ax3.legend([nn, lr, mo, train_set_err_plot, test_set_err_plot, lowest_err_plot],
+                   [nn_label, lr_label, mo_label, 'Avg. Error of Epoch', 'Avg. Error of Test Set', 'Lowest NN Error'],
+                   prop={'size': 14})
 
 
 def main():
@@ -170,7 +190,7 @@ def main():
     data = [[float(w) for w in l.split()] for l in file.readlines()]
     print('Loaded ', len(data), 'data sets.')
     random.shuffle(data)
-    train_set, test_set = split_to_test_and_training(data, 0.7)
+    # k folds
 
     # # Test LR 1 from 0.2435 to 0.2126 in 500 epochs
     # Note oscillates
@@ -182,8 +202,10 @@ def main():
     # net.train(500, train_set, test_set, 0.15, 0.00)
 
     # # Test LR 3 from 0.2611 to 0.2349 in 250 epochs
-    # net = Network(sigmoid, 2, [5], 1)
-    # net.train(250, train_set, test_set, 0.10, 0.00)
+    net = Network(sigmoid, 2, [5, 10], 1)
+    print('before', net.biases)
+    net.train(100, data, 0.25, 0.075)
+    print('after', net.biases)
 
     # # Test LR 4 from 0.2611 to 0.2349 in 250 epochs
     # net = Network(sigmoid, 2, [5], 1)
@@ -206,9 +228,14 @@ def main():
     # net = Network(sigmoid, 2, [10], 1)
     # net.train(250, train_set, test_set, 0.2, 0.2)
 
-    # # Test from 0.2611 to 0.2349 in 250 epochs
-    net = Network(sigmoid, 2, [15], 1)
-    net.train(1200, train_set, test_set, 0.18, 0.11)
+    # # Test
+    # net = Network(sigmoid, 2, [15], 1)
+    # net.train(250, train_set, test_set, 0.18, 0.11)
+
+    # # Test
+    # net = Network(sigmoid, 2, [15], 1)
+    # net.train(250, train_set, test_set, 0.18, 0.11)
+
     plt.show()
 
 
